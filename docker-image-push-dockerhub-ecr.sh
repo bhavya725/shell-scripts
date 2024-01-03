c73a5fbbf337b222c238bffca6affc791bafe5dd
bhavya725 <78220730+bhavya725@users.noreply.github.com>
2024-01-03 17:09:30 +0530

    adding comment

--- docker-image-push-dockerhub-ecr.sh
+++ docker-image-push-dockerhub-ecr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 docker images
+#docker login command and password is passed through a local variable
 docker login --Username bhavyapandey1210 --Password $password
 docker tag dummy_image:latest bhavyapandey1210/dummy_image:v1
 docker push bhavyapandey1210/dummy_image:v1
 docker logout
+#docker ecr login
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 775422423362.dkr.ecr.ap-south-1.amazonaws.com
 docker tag dummy_image:latest 775422423362.dkr.ecr.ap-south-1.amazonaws.com/bhavya:latest
 docker push 775422423362.dkr.ecr.ap-south-1.amazonaws.com/bhavya:latest
